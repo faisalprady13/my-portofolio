f7aa1f6d9ca486695c8865e9bc14f082d7a63f5a
Faisal Prady <faisalprady@faisals-mbp.localdomain>
2025-01-09 13:54:48 +0100

    redeploy

--- src/components/contacts.tsx
+++ src/components/contacts.tsx
@@ -8,7 +8,11 @@ const Contacts = () => {
       <div className="pt-20 pb-32 px-8 max-w-4xl m-auto">
         <p className="text-center font-bold text-lg mb-24 px-6">CONTACTS</p>
         <div className="flex flex-col md:flex-row flex-wrap items-center justify-evenly w-full gap-4 md:gap-12 max-w-4xl mx-auto py-4 text-2xl">
-          <a href="mailto:faisalprady@gmail.com" className={buttonStyle}>
+          <a
+            href="mailto:faisalprady@gmail.com"
+            className={buttonStyle}
+            target="_blank"
+          >
             E-mail
           </a>
           <a
@@ -27,7 +31,7 @@ const Contacts = () => {
           </a>
         </div>
       </div>
-      <div className="flex justify-between gap-4 px-12 text-xs" target="_blank">
+      <div className="flex justify-between gap-4 px-12 text-xs">
         <div>© Muhammad Faisal Ibnu Prady {new Date().getFullYear()}</div>
         <div>faisalpady@gmail.com</div>
       </div>
