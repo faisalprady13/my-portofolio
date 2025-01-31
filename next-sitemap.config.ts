037db597d9d15e8e4d665a9042a6cb635433df89
Faisal Prady <faisalprady@Faisals-MacBook-Pro.local>
2025-01-31 16:58:21 +0100

    improve seo

---
+++ next-sitemap.config.ts
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://faisalprady.vercel.app/',
+  generateRobotsTxt: true,
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="hydrated scroll-smooth">
+      <head>
+        <title>Faisal Prady | Portfolio</title>
+        <meta
+          name="description"
+          content="Faisal Prady - Full Stack Developer Portfolio"
+        />
+        <meta
+          name="keywords"
+          content="Faisal Prady, Web Developer, Portfolio, Next.js, Frontend Developer, Frontend Engineer"
+        />
+        <meta name="author" content="Faisal Prady" />
+        <meta property="og:title" content="Faisal Prady | Portfolio" />
+        <meta property="og:image" content="/me.png" />
+        <meta
+          property="og:description"
+          content="A showcase of my projects and skills in web development."
+        />
+        <meta property="og:url" content="https://faisalprady.vercel.app/" />
+      </head>
       <body
         cz-shortcut-listen="true"
         className={`${raleway.variable} antialiased relative`}
